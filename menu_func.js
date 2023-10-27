0b6f3b1f08cb0bbea949e9cac87095ea87f4f104
StephenKarim <stephen.karim07@gmail.com>
2023-10-27 16:35:51 -0400

    index style

--- menu_func.js
+++ menu_func.js
@@ -3,26 +3,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const topicsButton = document.getElementById("topics-button");    
     const aboutButton = document.getElementById("about-button");           
     const faqButton = document.getElementById("faq-button");           
-    const resourcesButton = document.getElementById("get-started-button"); 
+    const resourcesButton = document.getElementById("resource-container"); 
     const menuButton = document.getElementById("menu-button");
     const menuOverlay = document.getElementById("menu-overlay");
     const closeMenuLink = document.getElementById("close-menu");
+    const chatBotButton =  document.getElementById("chatbot-container");
 
-        // Event listener for the "Menu" button click
-        menuButton.addEventListener("click", function () {
-            // Toggle the active class to show/hide the overlay
-            menuOverlay.classList.toggle("active");
-        });   
-        
-        closeMenuLink.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent the default link behavior (navigating)
-            // Close the menu by removing the active class
-            menuOverlay.classList.remove("active");
-          });
+    function fadeAndRedirect(destination) {
+        // Fade out the container
+        document.body.style.opacity = 0;
+        // Wait for the transition to complete
+        setTimeout(function () {
+          // Redirect to the destination
+          window.location.href = destination;
+        }, 0); // 300ms is the duration of the transition
+      }    
+
+    // Event listener for the "Menu" button click
+    menuButton.addEventListener("click", function () {
+        // Toggle the active class to show/hide the overlay
+        menuOverlay.classList.toggle("active");
+    });   
     
+    closeMenuLink.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the default link behavior (navigating)
+        // Close the menu by removing the active class
+        menuOverlay.classList.remove("active");
+        });
+
  
     contactButton.addEventListener("click", function () {
-        window.location.href = "contact.html";
+        fadeAndRedirect("contact.html");
     });          
 
     topicsButton.addEventListener("click", function () {
@@ -38,8 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     resourcesButton.addEventListener("click", function () {
-        window.location.href = "Resources.html";
+        fadeAndRedirect("Resources.html");
     });
 
-   
+    chatBotButton.addEventListener("click", function () {
+        window.location.href = "chatbot.html";
+      });
+
+      
+  
+       
 });
